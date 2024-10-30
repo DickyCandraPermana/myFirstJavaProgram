504541f340367b6213d59b8959fd13fcb67b8480
Dicky Candra Permana <candrapermanadicky@gmail.com>
2024-10-30 11:05:30 +0700

    here iam

--- MyFirstJavaProgram/src/uts/konversiUmur.java
+++ MyFirstJavaProgram/src/uts/konversiUmur.java
@@ -23,17 +23,17 @@ public class konversiUmur {
     int tahunLahir = Integer.parseInt(partsLahir[2]);
     int bulanLahir = Integer.parseInt(partsLahir[1]);
     int hariLahir = Integer.parseInt(partsLahir[0]);
-    int hariDariLahir = (tahunLahir * 365) + (bulanLahir * 30) + hariLahir;
+    int hariDariLahir = (tahunLahir * 30 * 12) + (bulanLahir * 30) + hariLahir;
 
     int tahunSekarang = Integer.parseInt(partsSekarang[2]);
     int bulanSekarang = Integer.parseInt(partsSekarang[1]);
     int hariSekarang = Integer.parseInt(partsSekarang[0]);
-    int hariDariSekarang = (tahunSekarang * 365) + (bulanSekarang * 30) + hariSekarang;
+    int hariDariSekarang = (tahunSekarang * 30 * 12) + (bulanSekarang * 30) + hariSekarang;
 
     int selisihHari = hariDariSekarang - hariDariLahir;
 
-    int umurTahun = selisihHari / 365;
-    selisihHari = selisihHari % 365;
+    int umurTahun = selisihHari / (12 * 30);
+    selisihHari = selisihHari % (12 * 30);
     int umurBulan = selisihHari / 30;
     int umurHari = selisihHari % 30;
 
